ab99d01c3e525f779fd7b27192bc540af82f35c6
Ricky dildy <rdildy92@gmail.com>
2019-11-05 04:59:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,5 @@
 # Given a hash with numeric values, return the key for the smallest value
 bat_family = {"Batman" => 45, "Robin" => 17, "Batgirl" => 19}
 def key_for_min_value(bat_family)
-    #expect bat_family.to receive
+    expect (bat_family).to return lowest value
 end
